64a19acbaad46c97f0cfc467ac401a54e077d4d7
Tomas Ricaurte <TomasRicaurte.19@gmail.com>
2023-08-16 16:52:12 -0500

    Lista de contactos casi completa

--- ListaDeContactos.js
+++ ListaDeContactos.js
@@ -10,4 +10,28 @@ let ListaDeContactos = [
     "Ruben Ricaurte",
     "Florinda Ososrio",
     "Fanny Rodriguez"
-]
+];
+
+// Funcion para agregar un elemento a la lista
+
+function AgregarContacto(contacto, lista) {
+    lista.push(contacto);
+}
+AgregarContacto("Elena Manrique", ListaDeContactos)
+
+// Funcion para eliminar un contacto de la lista
+
+function QuitarContacto(contacto, lista) {
+    let indice = lista.indexOf(contacto);
+    if (indice !== -1){
+        lista.splice(indice, 1);
+        console.log("El contacto ${contacto} ha sido eliminado.");
+    } else {
+        console.log("El contacto ${contacto} no existe en la lista.")
+    }
+}
+
+QuitarContacto("David Gonzales", ListaDeContactos)
+
+console.log(ListaDeContactos)
+
