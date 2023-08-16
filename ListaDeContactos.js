6f0d53132843e52602eaf7ce05034705f3a5134c
Tomas Ricaurte <TomasRicaurte.19@gmail.com>
2023-08-16 13:50:28 -0500

    Lista de contacto inicial

---
+++ ListaDeContactos.js
@@ -0,0 +1,13 @@
+let ListaDeContactos = [
+    "Fabian Obando",
+    "Juan Cantillo",
+    "Laura Sanchez",
+    "David Gonzales",
+    "Camila Quintero",
+    "Claudia Velazco",
+    "Nicolas Zuluaga",
+    "Jose Ricaurte",
+    "Ruben Ricaurte",
+    "Florinda Ososrio",
+    "Fanny Rodriguez"
+]
