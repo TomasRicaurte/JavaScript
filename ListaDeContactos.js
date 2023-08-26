53ae30c5dadae73ba2419c98b8a62c816049bcbf
Tomas Ricaurte <TomasRicaurte.19@gmail.com>
2023-08-25 22:16:15 -0500

    Cambios para mejorar la funcionalidad

--- ListaDeContactos.js
+++ ListaDeContactos.js
@@ -5,16 +5,16 @@ let ListaDeContactos = [ {
     nombres: "Juanito",
     apellidos: "alcachofa",
     telefono: 3216540879,
-    ubicacion: "La Habana, Cuba"
+    ubicacion: {Ciudad:"La Habana, Cuba", direccion:"Julio A Mella - Maceo"}
 }, {
     id: 2,
     nombres: "Pedro",
     apellidos: "Navaja",
     telefono: 3216540879,
-    ubicacion: "San juan, Puerto rico"
+    ubicacion: {Ciudad:"San Juan, Puerto Rico", direccion:"Calle 6 - calle 13"}
 }
 
-];
+]
 
 // Funcion para agregar un elemento a la lista
 
@@ -33,8 +33,8 @@ function AgregarContacto(id, nombres, apellidos, telefono, ubicacion) {
 
 // Funcion para eliminar un contacto de la lista
 
-function QuitarContacto(id) {
-    let indice = ListaDeContactos.findIndex(contacto => contacto.id === id);
+function QuitarContacto(direccion) {
+    let indice = ListaDeContactos.findIndex(contacto => contacto.ubicacion.direccion === direccion);
     
     if (indice !== -1){
         let ContactoEliminado = ListaDeContactos.splice(indice, 1)
@@ -47,7 +47,7 @@ function QuitarContacto(id) {
 // Funcion para actualizar datos de contactos
 
 function actualizarContacto(id, nuevosDatos) {
-    const indice = ListaDeContactos.findIndex(contacto => contacto.id === id)
+    let indice = ListaDeContactos.findIndex(contacto => contacto.id === id)
 
     if (indice !== -1) {
         ListaDeContactos[indice] = {...ListaDeContactos[indice], ...nuevosDatos};
@@ -57,11 +57,17 @@ function actualizarContacto(id, nuevosDatos) {
 }
 
 // Ejemplo de uso
-AgregarContacto(3, "Laura", "Sanchez", 1234567890, "Bogota, Kennedy")
-AgregarContacto(4, "Tomas", "Ricaurte", 9876543210, "Bogota, Fontibon")
+AgregarContacto(3, "Laura", "Sanchez", 1234567890, {ciudad:"Bogota, Kennedy", direccion:"cl 26 sur #70h"})
+AgregarContacto(4, "Tomas", "Ricaurte", 9876543210, {Ciudad:"Bogota, Fontibon", direccion:"cl 25c #85b"})
 
-let ContactoEliminado = QuitarContacto()
+let ContactoEliminado = QuitarContacto("cl 26 sur #70h")
 
 actualizarContacto(2, {telefono: 6789054321})
 
-console.log(ListaDeContactos)
+function ImprimirContactos() {
+    console.log(ListaDeContactos)
+}
+
+//console.log(ListaDeContactos)
+
+ImprimirContactos()
